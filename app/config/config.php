bd16f70ed994968d66bbd2b09426dae08a6eb392
MuhamedKhX <muhamedkhx2@gmail.com>
2021-06-05 07:38:58 +0200

    first steps

--- app/config/config.php
+++ app/config/config.php
@@ -4,13 +4,13 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_NAME', 'shareposts');
 
 //App Root
 define('APPROOT' , dirname(dirname(__FILE__)));
 
 //URL Root
-define('URLROOT' , 'http://mvc.mv');
+define('URLROOT' , 'http://shareposts.sh');
 
 //Site Name
-define('SITENAME', 'Framework');
+define('SITENAME', 'SharePosts');
