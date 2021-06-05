db92443566e44084c4761ac090a4a3cacb45836b
MuhamedKhX <muhamedkhx2@gmail.com>
2021-06-05 07:21:42 +0200

    some fixing

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,10 +4,6 @@
 require_once("config/config.php");
 
 //Load Libraries
-/*require_once("libraries/Core.php");
-require_once("libraries/Controller.php");
-require_once("libraries/Database.php");*/
-
 
 spl_autoload_register(function ($ClassName)
 {
